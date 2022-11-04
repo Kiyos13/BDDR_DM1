6967165e14e57ec32723093678cb730225d15347
Kiyos13 <mathilde.pommier.13@gmail.com>
2022-11-04 15:42:10 -0400

    Ajoutez des fichiers projet.

---
+++ BDDR_DM1/ListCS.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BDDR_DM1
+{
+    /// <summary>
+    /// Logique d'interaction pour ListCS.xaml
+    /// </summary>
+    public partial class ListCS : Window
+    {
+        public ListCS()
+        {
+            InitializeComponent();
+        }
+
+        void CS_R1_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        void CS_R2_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        void CS_R3_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        void CS_R4_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        void CS_R5_Click(object sender, RoutedEventArgs e)
+        {
+        }
+    }
+}
